6b58f7eac90dd0c805708da1bda8d4611263e923
Alex Trimpe <alexwtrimpe@gmail.com>
2024-07-26 22:31:54 -0400

    Adding all legacy files

---
+++ src/routes/gas/+page.svelte
@@ -0,0 +1,53 @@
+<table>
+	<tr>
+		<th>Station</th>
+		<th>Code</th>
+	</tr>
+	<tr>
+		<td>BP</td>
+		<td>2416305297550713069</td>
+	</tr>
+	<tr>
+		<td>Marathon</td>
+		<td>11-812-390-0942</td>
+	</tr>
+	<tr>
+		<td>Shell</td>
+		<td>812-390-0942</td>
+	</tr>
+	<tr>
+		<td>Speedway</td>
+		<td>ALT ID - 432446359648</td>
+	</tr>
+	<tr>
+		<td>Pilot Flying J</td>
+		<td>9031 2164 9603 95</td>
+	</tr>
+</table>
+
+<style>
+	table {
+		margin: auto;
+		border-collapse: collapse;
+		width: 100%;
+		font-size: 32px;
+	}
+
+	td,
+	th {
+		padding: 1em 2em;
+	}
+
+	td {
+		border: none;
+	}
+
+	th {
+		color: white;
+		background-color: black;
+	}
+
+	tr:nth-child(odd) {
+		background-color: #eeeeee;
+	}
+</style>

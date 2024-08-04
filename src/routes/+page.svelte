cdac5da362dfd7a58d5b57cb8d4f981039f7c0c7
Alex Trimpe <alexwtrimpe@gmail.com>
2024-08-03 21:27:12 -0400

    Major design changes. Dialogs, colors, sections, etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
 	import Angular from '../images/angular.svelte';
 	import CSharp from '../images/c-sharp.svelte';
 	import Express from '../images/express.svelte';
@@ -46,61 +47,116 @@
 		}
 	];
 	const year = new Date().getFullYear();
+
+	let dialog: HTMLDialogElement;
+
+	onMount(async () => {
+		document.title = 'Alex W. Trimpe';
+		dialog = document.getElementById('confirmation-dialog') as HTMLDialogElement;
+	});
+
+	// Show the dialog when clicking "Delete everything"
+	const showDialogClick = () => {
+		dialog.show();
+	};
+
+	const closeClick = () => {
+		dialog.close();
+	};
 </script>
 
 <body>
 	<h1 id="title">alexwtrimpe.com</h1>
 	<main>
-		<h2>Background</h2>
-		<p>
-			I am a spring 2018 graduate of Indiana University's School of Informatics, Computing,
-			and Engineering. After graduation, I started working for Ford Motor Company in their
-			rotational college graduate program. During that time I also acquired my Master's of
-			Business Administration from the University of Michigan - Dearborn.
-		</p>
-		<h2>Expertise</h2>
-		<p>
-			Over the years I have learned quite a few technologies. Some I am more knowledgeable in
-			than others, while others I have high proficiency. Here are a few that I most enjoy.
-		</p>
-		<div class="technologies">
-			<div class="tech">
-				{#each images as image}
-					<span class="icon">
-						<svelte:component this={image} />
-					</span>
+		<section>
+			<h2>Background</h2>
+			<p>
+				I am a spring 2018 graduate of Indiana University's School of Informatics,
+				Computing, and Engineering. After graduation, I started working for Ford Motor
+				Company in their rotational college graduate program. During that time I also
+				acquired my Master's of Business Administration from the University of Michigan -
+				Dearborn.
+			</p>
+		</section>
+
+		<section>
+			<h2>Expertise</h2>
+			<p>
+				Over the years I have learned quite a few technologies. Some I am more knowledgeable
+				in than others, while others I have high proficiency. Here are a few that I most
+				enjoy.
+			</p>
+			<div class="technologies">
+				<div class="tech">
+					{#each images as image}
+						<span class="icon">
+							<svelte:component this={image} />
+						</span>
+					{/each}
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<h2>About this Site</h2>
+			<p>
+				This site was created using <a href="https://kit.svelte.dev/" target="_blank"
+					>SvelteKit</a
+				>. The desire was to reduce server costs to a minimum and maximize efficiency of
+				load time. To do this I:
+			</p>
+
+			<ul>
+				<li>Favored embedded SVGs over image files</li>
+				<li>Favored CSS over JS</li>
+				<li>Opted for client-side rendering</li>
+			</ul>
+			<button on:click={() => showDialogClick()}>Read More</button>
+			<dialog id="confirmation-dialog">
+				<div class="close-button">
+					<button on:click={closeClick}>&#10006;</button>
+				</div>
+				<p>
+					This site was made using Svelte structured for client-side only. The reason for
+					using Svelte was because of its simplicity to get started, scalability, and
+					small size. I also particularly am a fan of SvelteKit enforcing <a
+						href="https://kit.svelte.dev/docs/accessibility">accessibility</a
+					> rules by default.
+				</p>
+				<p>
+					I chose to not include an project dependencies outside of the standard
+					SvelteKit. This is to reduce the risk of vulnerabilities in 3rd-party packages,
+					as well as reduce the risk of the site breaking during version upgrades. Overall
+					this should limit the long-term maintenance effort required.
+				</p>
+				<p>While I am a huge fan of various design systems,</p>
+				<ul>
+					Here are some of my favorites design systems if you need one
+					<li><a href="https://primedesignsystem.com/" target="_blank">Prime 6</a></li>
+					<li><a href="https://m3.material.io/" target="_blank">Material 3</a></li>
+					<li>
+						<a href="https://atlassian.design/" target="_blank">Atlassian Design</a>
+					</li>
+				</ul>
+				<p>
+					What that means though, is that for better or worse, what you see here was
+					developed all on my own.
+				</p>
+			</dialog>
+		</section>
+
+		<section>
+			<h2>Contact</h2>
+			<p>Feel free to contact me at any time</p>
+			<div class="contact">
+				{#each contacts as contact}
+					<a class="icon" href={contact.link} target="_blank">
+						<svelte:component this={contact.image} />
+						<p>{contact.text}</p>
+					</a>
 				{/each}
 			</div>
-		</div>
-
-		<h2>About this Site</h2>
-		<p>
-			Overall, this site sits at only kilobytes in size. The goal was create a page that would
-			be responsive, but would be extremely small. The desire was to reduce server costs to a
-			minimum and maximize efficiency of load time. To do this I:
-		</p>
-
-		<ul>
-			<li>Used only a single HTML file for the site</li>
-			<li>Favored embedded SVGs over image files</li>
-			<li>Favored CSS over JS</li>
-			<li>Minified the file before deployment</li>
-		</ul>
-		<p>
-			Of course there is tooling and frameworks that can make this easier, but I kept this
-			basic and simple to reduce any pain in long-term maintenance
-		</p>
-
-		<h2>Contact</h2>
-		<p>Feel free to contact me at any time</p>
-		<div class="contact">
-			{#each contacts as contact}
-				<a class="icon" href={contact.link} target="_blank">
-					<svelte:component this={contact.image} />
-					<p>{contact.text}</p>
-				</a>
-			{/each}
-		</div>
+		</section>
 	</main>
 </body>
 
@@ -112,24 +168,7 @@
 </footer>
 
 <style>
-	body {
-		background-color: #201e22;
-		color: white;
-		font-family: Arial, sans-serif;
-	}
-
-	main {
-		background-color: #515151;
-		border-radius: 15px;
-		margin: 2em 1em;
-		padding: 2em;
-	}
-
-	footer {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+	@import './style.css';
 
 	#title {
 		margin: 0;
